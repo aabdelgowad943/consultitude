d902922dcc6d5235c6496a8dd10054a9540bd441
aabdelgowad943 <131953551+aabdelgowad943@users.noreply.github.com>
2024-12-27 02:56:49 +0200

    create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'consultitude';
+  constructor(private primeng: PrimeNG) {
+    this.primeng.theme.set({
+      preset: Aura,
+      options: {
+        cssLayer: {
+          name: 'primeng',
+          order: 'tailwind-base, primeng, tailwind-utilities',
+        },
+      },
+    });
+  }
 }
