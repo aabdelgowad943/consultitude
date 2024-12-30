3c142a0551429a858690b459e2795aaeadcf45b3
aabdelgowad943 <131953551+aabdelgowad943@users.noreply.github.com>
2024-12-30 10:35:53 +0200

    add logo component to hero section and include SVG asset

--- src/app/features/landing/components/hero-section/hero-section.component.ts
+++ src/app/features/landing/components/hero-section/hero-section.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FlexLogoComponent } from '../flex-logo/flex-logo.component';
 import { SubscribeFormComponent } from '../subscribe-form/subscribe-form.component';
+import { LogoComponent } from '../../../../shared/logo/logo.component';
 
 @Component({
   selector: 'app-hero-section',
-  imports: [FlexLogoComponent, SubscribeFormComponent],
+  imports: [FlexLogoComponent, SubscribeFormComponent, LogoComponent],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss',
 })
