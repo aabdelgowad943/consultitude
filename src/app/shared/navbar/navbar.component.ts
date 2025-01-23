432f4a38549f5a8fa40b903bbb7b04d6f7984acb
aabdelgowad943 <131953551+aabdelgowad943@users.noreply.github.com>
2025-01-23 02:47:38 +0200

    solving bugs

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -13,5 +13,24 @@ export class NavbarComponent {
 
   toggleMenu() {
     this.menuVisible = !this.menuVisible;
+  }
+
+  constructor(private router: Router) {}
+
+  // Getter to determine if red background should be applied
+  get shouldApplyRedBg(): boolean {
+    const currentUrl = this.router.url;
+    const targetPaths = [
+      'privacy-and-policy',
+      'about-us',
+      'terms-and-conditions',
+      'cookie-policy',
+    ];
+    return targetPaths.some((path) => currentUrl.includes(path));
+  }
+
+  // Check if current route is home
+  get isHomePath(): boolean {
+    return this.router.url === '/home';
   }
 }
