6bde4432d3b7803424b9eecc15a7d68d28043cda
aabdelgowad943 <131953551+aabdelgowad943@users.noreply.github.com>
2025-02-11 02:30:21 +0200

    feat: add language selection and random image functionality in product card

--- src/app/features/knowledge-hub/services/product-service.service.ts
+++ src/app/features/knowledge-hub/services/product-service.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { map, Observable, tap } from 'rxjs';
 import { ApiService } from '../../../../shared/services/api.service';
 import { Product, ProductStatus } from '../models/products';
+import { Language } from '../models/language.enum';
 
 @Injectable({
   providedIn: 'root',
@@ -14,18 +15,20 @@ export class ProductServiceService {
     page: number = 1,
     limit: number = 10,
     statuses: ProductStatus[] = [ProductStatus.ACTIVE, ProductStatus.ARCHIVED],
-    language: string = 'EN',
+    language: Language = Language.EN,
     search: string = '',
     sortBy: string = '',
     areaOfFocus: string[] = [],
+    features: string[] = [],
     domains: string[] = [],
     documentTypes: string[] = [],
     minPrice: number = 0,
-    maxPrice: number = 10000
+    maxPrice: number = 1000
   ): Observable<{ products: Product[]; totalPages: number }> {
     let params = new HttpParams()
       .set('page', page.toString())
-      .set('limit', limit.toString());
+      .set('limit', limit.toString())
+      .set('language', language); // Add language as a query parameter
 
     if (statuses?.length) {
       params = params.set('statuses', statuses.join(','));
@@ -37,18 +40,21 @@ export class ProductServiceService {
       params = params.set('sortBy', sortBy);
     }
     if (areaOfFocus.length) {
-      params = params.set('areaOfFocus', areaOfFocus.join(','));
+      params = params.set('areaOfFocusIds', areaOfFocus.join(','));
     }
     if (domains.length) {
       params = params.set('domainIds', domains.join(','));
     }
     if (documentTypes.length) {
-      params = params.set('documentTypes', documentTypes.join(','));
+      params = params.set('documentFormatIds', documentTypes.join(','));
+    }
+    if (features.length) {
+      params = params.set('featureIds', documentTypes.join(','));
     }
     params = params.set('minPrice', minPrice.toString());
     params = params.set('maxPrice', maxPrice.toString());
 
-    const headers = new HttpHeaders().set('Accept-Language', language);
+    const headers = new HttpHeaders().set('Accept-Language', 'EN'); // Set Accept-Language header
 
     return this.apiService
       .get<{ data: Product[]; meta: { totalPages: number } }>('/products', {
@@ -112,6 +118,23 @@ export class ProductServiceService {
           response.data.map((item: any) => ({
             id: item.documentFormatId || item.id || null,
             name: item.name,
+            checked: false,
+          }))
+        )
+      );
+  }
+
+  getAllFeatures(page: number = 1, limit: number = 100): Observable<any[]> {
+    return this.apiService
+      .get<any>(`/features?page=${page}&limit=${limit}`)
+      .pipe(
+        map((response) =>
+          response.data.map((item: any) => ({
+            id: item.featureId || item.id || null,
+            name:
+              item.translations && item.translations.length
+                ? item.translations[0].name
+                : '',
             checked: false,
           }))
         )
