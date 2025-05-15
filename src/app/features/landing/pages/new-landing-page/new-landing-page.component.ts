28cdd28bbf880fc6da2cb90e96d9389273e77e7d
aabdelgowad943 <131953551+aabdelgowad943@users.noreply.github.com>
2025-05-15 04:45:50 +0300

    feat: Update layout and styling for new landing page components; enhance footer background color

--- src/app/features/landing/pages/new-landing-page/new-landing-page.component.ts
+++ src/app/features/landing/pages/new-landing-page/new-landing-page.component.ts
@@ -3,6 +3,9 @@ import { NavbarComponent } from '../../../../shared/navbar/navbar.component';
 import { HeroSectionNewLandingComponent } from './components/hero-section-new-landing/hero-section-new-landing.component';
 import { FooterComponent } from '../../../../shared/footer/footer.component';
 import { FeaturesComponent } from '../index/components/features/features.component';
+import { FaqsComponent } from '../index/components/faqs/faqs.component';
+import { CardComponent } from './components/card/card.component';
+import { ThreeCardsComponent } from './components/three-cards/three-cards.component';
 
 @Component({
   selector: 'app-new-landing-page',
@@ -11,6 +14,9 @@ import { FeaturesComponent } from '../index/components/features/features.compone
     HeroSectionNewLandingComponent,
     FooterComponent,
     FeaturesComponent,
+    FaqsComponent,
+    CardComponent,
+    ThreeCardsComponent,
   ],
   templateUrl: './new-landing-page.component.html',
   styleUrl: './new-landing-page.component.scss',
